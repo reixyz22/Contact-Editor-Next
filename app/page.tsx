f36fffe00ef0da0975ec576576feafa9fc59f193
reixyz22 <spellcasters22@gmail.com>
2024-06-24 02:01:55 -0500

    fixed prisma build script and added a new homepage that imports contacts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+'use client';
 import React from 'react';
+import Contacts from './contacts/page';
 
 const Page: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to my page!</h1>
+        <h1 style={{color: 'darkblue'}}>Hello New. World</h1>
+        <Contacts/>
     </div>
   );
 };
